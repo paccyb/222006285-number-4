6c79d88b7a29752bf800148ae21cb9ca278a7af6
paccyb <149565925+paccyb@users.noreply.github.com>
2023-11-06 00:59:35 +0200

    Add files via upload

---
+++ dowhile2.java
@@ -0,0 +1,13 @@
+// for counting ascending ordeer
+
+public class dowhile2 {// public class called dowhile2
+    public static void main(String[] args) {// main method
+        int h=1;
+        do{
+        System.out.println(h);// out put statement
+        h++;}
+        while(h<=10);// while loop statement
+        }    
+        }  
+    
+
