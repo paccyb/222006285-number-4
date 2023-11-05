e3f2935b0a941552b56912abf500e988f72c2c10
paccyb <149565925+paccyb@users.noreply.github.com>
2023-11-06 00:56:34 +0200

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,12 @@
+// displaying word bit several times by do while loop
+
+public class dowhile{ // public class dowhile
+public static void main(String[] args) {
+int h=1;
+do{
+System.out.println("BIT");// out put statement for do
+h++;}// incrementation statement
+while(h<=10);// while loop
+}    
+}  
+
