8a73f4aeb378be9bd74059f394000b6ffc1d95aa
paccyb <149565925+paccyb@users.noreply.github.com>
2023-11-06 00:08:32 +0200

    Add files via upload

---
+++ switchexample.java
@@ -0,0 +1,19 @@
+// program uses switch case to display grades
+public class switchexample { // PUBLIC CLASS SWITCH
+ public static void main(String[]args){// MAIN METHOD
+int marks=65;
+switch (marks) {
+    case 65: System.out.println("GET GRADE A");
+    break;
+    case 60: System.out.println("GET GRADE B");
+    break;
+    case 55: System.out.println("GET GRADE C");
+    break;
+    case 50: System.out.println("GET GRADE D");
+    break;
+    default: System.out.println("UN CLASSIFIED");
+    
+        
+}
+ }   
+}
